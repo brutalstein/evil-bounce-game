1163e725cf0d11e4154e21cf4d4f906811c6792a
Cenker <ustacenker16@gmail.com>
2025-08-09 21:49:47 +0300

    update

--- scripts/setBallSpeed.js
+++ scripts/setBallSpeed.js
@@ -1,31 +1,31 @@
-function ball_speed_change(score, ballPhysics) {
+function ball_speed_change(score, ball) {
     if (score <= 5) {
-        ballPhysics.velocityX *= 1.1;
-        ballPhysics.velocityY *= 1.1;
+        ball.vx *= 1.1;
+        ball.vy *= 1.1;
     } else if (5 < score <= 10) {
-        ballPhysics.velocityX *= 1.04;
-        ballPhysics.velocityY *= 1.04;
+        ball.vx *= 1.04;
+        ball.vy *= 1.04;
     } else if (10 < score <= 15) {
-        ballPhysics.velocityX *= 1;
-        ballPhysics.velocityY *= 1;
+        ball.vx *= 1;
+        ball.vy *= 1;
     } else if (15 < score <= 20) {
-        ballPhysics.velocityX *= 1.03;
-        ballPhysics.velocityY *= 1.03;
+        ball.vx *= 1.03;
+        ball.vy *= 1.03;
     } else if (20 < score <= 25) {
-        ballPhysics.velocityX *= 1;
-        ballPhysics.velocityY *= 1;
+        ball.vx *= 1;
+        ball.vy *= 1;
     } else if (25 < score <= 30) {
-        ballPhysics.velocityX *= 1.02;
-        ballPhysics.velocityY *= 1.02;
+        ball.vx *= 1.02;
+        ball.vy *= 1.02;
     } else if (30 < score <= 35) {
-        ballPhysics.velocityX *= 1;
-        ballPhysics.velocityY *= 1;
+        ball.vx *= 1;
+        ball.vy *= 1;
     } else if (35 < score <= 40) {
-        ballPhysics.velocityX *= 1.01;
-        ballPhysics.velocityY *= 1.01;
+        ball.vx *= 1.01;
+        ball.vy *= 1.01;
     } else {
-        ballPhysics.velocityX *= 1;
-        ballPhysics.velocityY *= 1;
+        ball.vx *= 1;
+        ball.vy *= 1;
     }
 
 }
